bf42994e16b9e24c53522f3fa8969e2c5b2b8abd
nacorsy <yossansr06@gmail.com>
2021-04-06 20:49:38 +0800

    Initial commit

---
+++ Metta/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Metta
+//
+//  Created by Yossan Sandi Rahmadi on 06/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
