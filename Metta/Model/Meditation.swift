3fffd1a854e0c12bfba5ecd2f3ef8853e27cfa33
nacorsy <yossansr06@gmail.com>
2021-04-08 12:56:47 +0800

    feat: create data model and its initial data

---
+++ Metta/Model/Meditation.swift
@@ -0,0 +1,18 @@
+//
+//  Meditation.swift
+//  Metta
+//
+//  Created by Yossan Sandi Rahmadi on 08/04/21.
+//
+
+import Foundation
+
+struct Meditation {
+    let id: Int
+    let name: String
+    let caption: String
+    let description: String
+    let audioFile: String
+    let totalSession: Int
+    var currentSession: Int
+}
---
+++ Metta/Model/MeditationData.swift
@@ -0,0 +1,14 @@
+//
+//  MeditationData.swift
+//  Metta
+//
+//  Created by Yossan Sandi Rahmadi on 08/04/21.
+//
+
+import Foundation
+
+let meditations = [
+    Meditation(id: 0, name: "Breathe", caption: "Learn to refocus our mind with patience and better understanding", description: "With successfully practicing this meditation, we also learn how to strengthen our intuition.", audioFile: "", totalSession: 0, currentSession: 0),
+    
+    Meditation(id: 1, name: "Happiness", caption: "Learn to stay happy, even if your environment doesn’t let you be.", description: "With successfully practicing this meditation, we also learn how to strengthen our relationship with ourselves and others.", audioFile: "", totalSession: 10, currentSession: 0)
+]
