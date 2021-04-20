da834dcaf5af06dda70f8af3bbe31d1c3c53bbf9
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2021-04-20 19:21:44 +0700

    Update LoadingViewController.swift

--- Metta/Controller/LoadingViewController.swift
+++ Metta/Controller/LoadingViewController.swift
@@ -5,4 +5,34 @@
 //  Created by Novi Gunawan on 14/04/21.
 //
 
-import Foundation
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    private var isOnboardingSeen: Bool!
+    
+    private let storageManager = StorageManager()
+    private let navigationManager = NavigationManager()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        isOnboardingSeen = storageManager.isOnBoardingSeen()
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showInitialScreen()
+    }
+    func showInitialScreen()   {
+      
+        if isOnboardingSeen {
+            //straight go to home-page
+            navigationManager.show(screen: .home, inController: self)
+        }
+        else{
+            //go to onboarding screen first
+            navigationManager.show(screen: .onBoarding, inController: self)
+        }
+    }
+  
+}
