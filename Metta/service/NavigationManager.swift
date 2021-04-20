b775ad2e5f8f35c580336498dde3dbcc95e96b62
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2021-04-20 19:20:30 +0700

    Update NavigationManager.swift
    Fix: update navigationManager

--- Metta/service/NavigationManager.swift
+++ Metta/service/NavigationManager.swift
@@ -5,4 +5,32 @@
 //  Created by Novi Gunawan on 14/04/21.
 //
 
-import Foundation
+import UIKit
+
+class NavigationManager {
+    //punya list enum Screen
+    enum Screen {
+        case onBoarding
+        case home
+        
+    }
+    
+    func show(screen: Screen, inController: UIViewController!) {
+        var viewController: UIViewController!
+        switch screen {
+        
+        case .onBoarding:
+            viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "onboardingViewController")
+            
+        case .home:
+            viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "NavigationController")
+            
+        }
+        
+        if let sceneDelegate = inController.view.window?.windowScene?.delegate as? SceneDelegate,
+           let window = sceneDelegate.window {
+            window.rootViewController = viewController
+            UIView.transition(with: window, duration: 0.5, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        }
+    }
+}
