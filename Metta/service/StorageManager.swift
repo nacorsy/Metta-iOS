3ea476599d116fff71d94504f37436a155013e48
Novita Gunawan <54393790+novgunawan@users.noreply.github.com>
2021-04-20 19:21:01 +0700

    Update StorageManager.swift
    Fix: update storageManager

--- Metta/service/StorageManager.swift
+++ Metta/service/StorageManager.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+class StorageManager {
+    //punya List berbentuk enum Key
+    enum Key: String {
+        case isOnBoardingSeen
+    }
+    
+    func isOnBoardingSeen() -> Bool {
+        UserDefaults.standard.bool(forKey: Key.isOnBoardingSeen.rawValue )
+    }
+    
+    func setOnBoardingSeen(){
+        UserDefaults.standard.set(true, forKey: Key.isOnBoardingSeen.rawValue)
+    }
+    
+    //Testing
+    func resetOnboardingSeen() {
+        UserDefaults.standard.set(false, forKey: Key.isOnBoardingSeen.rawValue)
+    }
+    
+}
